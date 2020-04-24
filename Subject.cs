65be1448c68ff4c5f0093dce012539df6aba55f1
David <davisbd100@gmail.com>
2020-04-23 22:54:13 -0500

    Agregado de funcion de llenado

--- Subject.cs
+++ Subject.cs
@@ -8,44 +8,53 @@ namespace EigthQueens
 {
     public class Subject
     {
-        public List<List<int>> Tablero { get; set; }
+        public int[][] Tablero { get; set; }
 
         public Subject()
         {
+            Tablero = new int[8][];
+            Tablero[0] = new int[8];
+            Tablero[0][7] = 5;
+            Tablero[0][6] = 7;
+            Tablero[0][2] = 1;
+            Tablero[1] = new int[8];
+            Tablero[2] = new int[8];
+            Tablero[3] = new int[8];
+            Tablero[4] = new int[8];
+            Tablero[5] = new int[8];
+            Tablero[6] = new int[8];
+            Tablero[7] = new int[8];
+            Console.WriteLine("Longitud del arreglo exterior: " + Tablero.Length + " Longitud del interior: " + Tablero[0].Length);
+            FillEmptySpaces(8);
+            Console.WriteLine("Longitud del arreglo exterior: " + Tablero.Length + " Longitud del interior: " + Tablero[0].Length);
 
         }
 
         public int CalculateAttitudeValue()
         {
             int fitnessValue = 0;
-            for (int i = 0; i < Tablero.Count; i++)
+            for (int i = 0; i < Tablero.Length; i++)
             {
-                for (int j = 0; j < Tablero[i].Count; j++)
+                for (int j = 0; j < Tablero[j].Length; j++)
                 {
                     if (Tablero[i][j] == 1)
                     {
-                        fitnessValue += GetCrossCollision(i, j);
+                        fitnessValue += GetHorizontalCollision(i,j);
                         fitnessValue += GetDiagonalCollision(i,j);
                     }
                 }
             }
             return fitnessValue;
         }
-        int GetCrossCollision(int i, int j)
+        int GetHorizontalCollision(int i, int j)
         {
             int collisionTimes = 0;
-            for (int k = i + 1; k < Tablero.Count; k++)
+
+            for (int l = j + 1; l < Tablero[i].Length; l++)
             {
-                if (Tablero[k][j] == 1)
+                if (Tablero[i][l] == 1)
                 {
                     collisionTimes++;
-                }
-                for (int l = j + 1; l < Tablero[i].Count; l++)
-                {
-                    if (Tablero[i][l] == 1)
-                    {
-                        collisionTimes++;
-                    }
                 }
             }
             return collisionTimes;
@@ -65,6 +74,38 @@ namespace EigthQueens
                 n++;
             }
             return collisionTimes;
+        }
+        void FillEmptySpaces(int size)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                int value = 0;
+                for (int j = 0; j < size; j++)
+                {
+                    bool exist = false;
+                    if (Tablero[i][j] == 0)
+                    {
+                        value++;
+                        for (int k = j+1; k < size; k++)
+                        {
+                            if (Tablero[i][k] == value)
+                            {
+                                value++;
+                                k = j + 1;
+                                exist = true;
+                            }
+                            else
+                            {
+                                exist = false;
+                            }
+                        }
+                        if (!exist)
+                        {
+                            Tablero[i][j] = value;
+                        }
+                    }
+                }
+            }
         }
     }
 }
