54f8cc804c3729a7a4420de7d15a497caa519e67
David <davisbd100@gmail.com>
2020-04-24 13:25:52 -0500

    Agregado de dependencia a PriorityQueue

---
+++ Population.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Priority_Queue;
+
+namespace EigthQueens
+{
+    public class Population
+    {
+        public float MutationProbability { get; set; }
+        public int PopulationSize { get; set; }
+        public int Parents { get; set; }
+        public int MaxEval { get; set; }
+        public SimplePriorityQueue MyProperty { get; set; }
+    }
+}
