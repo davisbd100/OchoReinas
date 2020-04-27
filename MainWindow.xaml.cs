71bff80aeafca35390e41ac0fa325e114e366b99
David <davisbd100@gmail.com>
2020-04-27 00:04:19 -0500

    Añadido de graficas de analisi

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media.Imaging;
+using LiveCharts;
+using LiveCharts.Wpf;
 
 namespace EigthQueens
 {
@@ -28,6 +30,7 @@ namespace EigthQueens
             population = new Population(int.Parse(tbPopulation.Text), int.Parse(tbBoard.Text), int.Parse(tbEvaluation.Text), double.Parse(tbMutation.Text), int.Parse(tbParents.Text));
             population.StartEvolutionProcess();
             FillGridGenerations();
+            SetCharts();
             if (tbBoard.Text == "8")
             {
                 SetBoard();
@@ -48,6 +51,55 @@ namespace EigthQueens
                 List<Image> images = gridBoard.Children.OfType<Image>().ToList();
                 images[row + value].Source = new BitmapImage(new Uri("Resources/Queen.png", UriKind.RelativeOrAbsolute));
             }
+        }
+
+        void SetCharts()
+        {
+            ChartValues<double> standardDeviationChart = new LiveCharts.ChartValues<double>();
+            ChartValues<double> mediaChart = new LiveCharts.ChartValues<double>();
+            ChartValues<double> bestValueChart = new LiveCharts.ChartValues<double>();
+            ChartValues<double> worstValueChart = new LiveCharts.ChartValues<double>();
+            foreach (var item in population.Generations)
+            {
+                standardDeviationChart.Add(item.StandardDeviation);
+                mediaChart.Add(item.Media);
+                bestValueChart.Add(item.BetterSubject.FitnessValue);
+                worstValueChart.Add(item.WorstSubject.FitnessValue);
+            }
+
+            SeriesCollection standardDeviationSeries =new SeriesCollection
+            {
+                new LineSeries
+                {
+                    Values = standardDeviationChart
+                }
+            };
+            SeriesCollection mediaSeries = new SeriesCollection
+            {
+                new LineSeries
+                {
+                    Values = mediaChart
+                }
+            };
+            SeriesCollection bestValueSeries = new SeriesCollection
+            {
+                new LineSeries
+                {
+                    Values = bestValueChart
+                }
+            };
+            SeriesCollection worstValueSeries = new SeriesCollection
+            {
+                new LineSeries
+                {
+                    Values = worstValueChart
+                }
+            };
+
+            StandarDeviationChart.Series = standardDeviationSeries;
+            MediaChart.Series = mediaSeries;
+            BestValueChart.Series = bestValueSeries;
+            WorstValueChart.Series = worstValueSeries;
         }
     }
 }
